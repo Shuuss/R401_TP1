c2436f8780118a0e4f0a50915a33eb0661d44b98
Tanguy ABDOULVAID <abdoulvt@iut-acy.local>
2025-01-28 08:45:48 +0100

    feat : model Devise.cs

---
+++ WSConvertisseur/WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,34 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string nomDevise;
+        private double taux;
+
+        public double Taux
+        {
+            get { return taux; }
+            set { taux = value; }
+        }
+
+
+        public string NomDevise
+        {
+            get { return nomDevise; }
+            set { nomDevise = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public Devise()
+        {
+               
+        }
+
+    }
+}
